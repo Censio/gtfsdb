79526332ee28ad794619d72a73fb0fd1a2dd02ff
Ryan G. Hunter <ryan@cens.io>
2015-09-26 19:53:35 -0400

    added delete feed

--- gtfsdb/model/metaTracking.py
+++ gtfsdb/model/metaTracking.py
@@ -27,12 +27,14 @@ class FeedFile(Base):
     completed = Column(Boolean, default=False)
     censio_upload_date = Column(DateTime)
 
-    agencies = relationship('GTFSExAgency',
+    ex_agencies = relationship('GTFSExAgency',
                             secondary=feed_agency_table,
                             backref='feeds')
+    agencies = relationship('Agency', backref="feed_file", primaryjoin='Agency.file_id==FeedFile.md5sum',
+                            foreign_keys='(Agency.file_id)', cascade='delete')
 
     def __init__(self, agencies, **kwargs):
-        self.agencies = [GTFSExAgency(dataexchange_id=agency) for agency in agencies]
+        self.ex_agencies = [GTFSExAgency(dataexchange_id=agency) for agency in agencies]
         if 'date_added' in kwargs.keys():
             kwargs['date_added'] = datetime.datetime.utcfromtimestamp(kwargs.get('date_added'))
         for key, value in kwargs.iteritems():
